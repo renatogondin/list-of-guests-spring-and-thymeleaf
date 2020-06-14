5ab0a3ec196b96a8f787903bbf2b28584380cd20
Renato Gondin <renatogondin1902@gmail.com>
2020-06-14 12:17:29 -0300

    last changes and deploying to heroku

--- src/main/java/com/renato/festa/model/Convidado.java
+++ src/main/java/com/renato/festa/model/Convidado.java
@@ -4,8 +4,10 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Convidado implements Serializable{
@@ -14,7 +16,8 @@ public class Convidado implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
 	
 	private String nome;
